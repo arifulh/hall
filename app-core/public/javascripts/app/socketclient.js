02ed8980eecd277d1492aab6d29aafd4bcb75a4e
Ariful Haque <arifulh@gmail.com>
2014-05-04 22:29:47 -0400

    setting up templates

---
+++ app-core/public/javascripts/app/socketclient.js
@@ -0,0 +1,28 @@
+﻿window.sclient = {
+
+	init: function() {
+        var self = this;
+        this.primus = Primus.connect('http://localhost:3000/primus', {
+			websockets: true,
+			network: true
+		});
+		self.primus.on('open', function () {
+            self.primus.write({ channel: 'test', message: 'news' });
+            self.primus.on('data', function(message){
+                console.log(message)
+            })
+		});
+	},
+
+    refresh: function() {
+        var self = this;
+        console.log(self)
+        self.primus.end();
+        self.primus = null;
+        setTimeout(function() {
+            self.init();
+        }, 100);
+    }
+
+};
+
